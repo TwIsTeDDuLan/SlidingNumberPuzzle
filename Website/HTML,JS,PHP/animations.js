775923668036d00ff349cd4129846b49a55b8deb
Thulana Dulan Jeewantha Amaranath <128315509+TwIsTeDDuLan@users.noreply.github.com>
2025-08-28 21:20:35 +0530

    Refactor game UI elements and styles for improved layout and functionality

--- Website/HTML,JS,PHP/animations.js
+++ Website/HTML,JS,PHP/animations.js
@@ -1,3 +1,18 @@
+//elements
+
+const playArea = document.querySelector('.play-area');
+const stat = document.querySelector('.stats');
+const mlAssist = document.querySelector('.ml-assist');
+const timeC = document.querySelector('.time');
+const moves = document.querySelector('.moves');
+const retry = document.querySelector('.retry');
+const board = document.querySelector('.board');/*board > div here as well*/
+
+const container = document.querySelector('.container');
+const header = document.querySelector('.header');
+const = document.querySelector('');
+
+
 function StartGame(){
-    return;
+
 }
